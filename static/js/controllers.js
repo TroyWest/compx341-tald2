701c3fb6e44dfa01af877c66608be57d699bba17
Troy Dean <tald2@students.waikato.ac.nz>
2019-05-16 23:11:47 +1200

    Revert "Minor changes"
    This reverts commit 994dd13c2ea7c19eaf79599e84fccf215784ff98.

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -22,7 +22,6 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
         var data = "";
         if(which === 1) {
             data = $scope.zip1m;
-            
         } else if(which === 2) {
             data = $scope.zip2m;
         } else if(which === 3) {
